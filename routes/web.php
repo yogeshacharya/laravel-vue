8fbcbea079594d33e2693d7a20c46490af023ae4
yogesh7 <yogacharya7@gmail.com>
2023-04-24 14:13:11 +0545

    fixes

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApplicationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/admin/dashboard', function () {
+//     return view('dashboard');
+// });
+
+Route::get('{view}', ApplicationController::class)->where('view','(.*)');
